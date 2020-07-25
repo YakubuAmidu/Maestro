202fe98f3a888ea9b8454504020bf7c079c0c00b
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-25 04:48:40 -0700

    Commiting changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      query: ' '
+      query: ' ',
+      artist: null
     }
   }
 
@@ -19,7 +20,11 @@ class App extends Component {
       method: 'GET'
     })
     .then(response => response.json())
-    .then(json => console.log('json', json));
+    .then(json => {
+      const artist = json.artists.items[0];
+      console.log('artist', artist);
+      this.setState({ artist });
+    });
   }
 
   render() {
