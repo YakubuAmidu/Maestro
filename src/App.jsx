9e570be33cd65c0c8300f085ac55dcb36ee1f791
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-25 04:33:59 -0700

    Commiting changes

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,11 @@ class App extends Component {
     const BASE_URL = 'https://api.spotify.com/v1/search?';
     const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
     console.log('FETCH_URL', FETCH_URL);
+    fetch(FETCH_URL, {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(json => console.log('json', json));
   }
 
   render() {
