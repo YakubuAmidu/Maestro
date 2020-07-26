9e29765e6c59eeb72306bdf711051dc0e79a6260
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-26 01:05:47 -0700

    Commiting changes

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,10 @@ class App extends Component {
   search() {
     console.log('this.state', this.state);
     const BASE_URL = 'https://api.spotify.com/v1/search?';
-    const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    let FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
     console.log('FETCH_URL', FETCH_URL);
+
     fetch(FETCH_URL, {
       method: 'GET'
     })
@@ -25,6 +27,15 @@ class App extends Component {
       const artist = json.artists.items[0];
       console.log('artist', artist);
       this.setState({ artist });
+
+      FETCH_URL = `${ALBUM_URL}/${artist.id}/top-tracks?country=US&`
+      fetch(FETCH_URL, {
+        method: 'GET'
+      })
+      .then(response => response.json())
+      .then(json => {
+        console.log('artist\'s top tracks:', json);
+      })
     });
   }
 
