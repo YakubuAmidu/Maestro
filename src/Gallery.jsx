ed9661746b8810a1559474659ae485d095a2474a
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-26 01:50:52 -0700

    Commiting changes

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -4,8 +4,28 @@ import './App.css';
 class Gallery extends Component {
   render() {
     console.log('gallery props', this.props);
+    const { tracks } = this.props;
     return (
-      <div>Gallery</div>
+      <div>
+         {tracks.map((track, k) => {
+           const trackImg = track.album.images[0].url;
+           return (
+             <div
+             key={k}
+             className="track"
+             >
+              <img
+              src={trackImg}
+              className="track-img"
+              alt="track"
+              />
+              <p className="track-text">
+               {track.name}
+              </p>
+             </div>
+           )
+         })}
+      </div>
     )
   }
 }
