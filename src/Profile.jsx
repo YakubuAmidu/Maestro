7ad461c69e3d40bcdd43b0c03ba75bcc782d1bbd
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-26 00:23:51 -0700

    Commiting changes

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -3,13 +3,31 @@ import './App.css';
 
 class Profile extends Component {
   render() {
-    let artist = { name: ' ', followers: {total: ' '}};
+    let artist = { name: ' ', followers: {total: ' '}, image: [{ url: ' '}], genres: [] };
     artist = this.props.artist !== null ? this.props.artist : artist;
 
     return (
      <div className="profile">
-      <div>{artist.name}</div>
-      <div>{artist.followers.total}</div>
+     <img
+      alt="Profile"
+      className="profile-img"
+      src={artist.images[0].url}
+     />
+
+     <div className="profile-info">
+      <div className="profile-name">{artist.name}</div>
+      <div className="profile-followers">{artist.followers.total}</div>
+      <div className="profile-genres">
+       {
+         artist.genres.map((genre, k) => {
+           genre = genre !== artist.genres[artist.genres.length-1] ? ` ${genre},` : ` & ${genre}`;
+           return (
+             <span key={k}>{genre}</span>
+           )
+         })
+       }
+       </div>
+      </div>
      </div>
     )
   }
