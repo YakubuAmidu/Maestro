582d3af73f83de8675d8eef5159d3a0e6d8d1bfb
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-09 00:30:58 -0700

    Updates

---
+++ precache-manifest.ca52e1ff78ba6188354c44f32875e869.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d842863936eab66e6196962913d11b",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "1a5e849e10726e81b4b9",
+    "url": "/my-app/static/css/main.5ae84b2a.chunk.css"
+  },
+  {
+    "revision": "dd275d181431efac20d0",
+    "url": "/my-app/static/js/2.9ff5af20.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/my-app/static/js/2.9ff5af20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a5e849e10726e81b4b9",
+    "url": "/my-app/static/js/main.c35867b7.chunk.js"
+  },
+  {
+    "revision": "25e2001e343066fdc3d5",
+    "url": "/my-app/static/js/runtime-main.d014726e.js"
+  }
+]);
